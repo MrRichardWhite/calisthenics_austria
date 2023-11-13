2f4eff2380abf84291091b22dd24ddbb3240a2f1
Richard Weiss <richard.weiss.official@gmail.com>
2023-11-14 00:16:06 +0100

    integrated code from repository `flutter_chat_app_ui`

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'config/theme.dart';
+import 'screens/screens.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chat App UI',
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+      home: const HomeScreen(),
+      getPages: [
+        GetPage(name: '/', page: () => const HomeScreen()),
+        GetPage(name: '/chat', page: () => const ChatScreen()),
+      ],
     );
   }
 }
